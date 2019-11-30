8e2323150f14cf91f84371c8c48d78eaac699a58
Allison Harrison <allisonrharrison@gmail.com>
2019-11-29 21:15:45 -0800

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { DISHES } from '../shared/dishes';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
   Menu: { screen: Menu },
@@ -22,27 +22,45 @@ const MenuNavigator = createStackNavigator({
 }
 );
 
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      color: "#fff"
+    }
+  }
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: {
+    screen: HomeNavigator,
+    navigationOptions: {
+      title: 'Home',
+      drawerLabel: 'Home'
+    }
+  },
+  Menu: {
+    screen: MenuNavigator,
+    navigationOptions: {
+      title: 'Menu',
+      drawerLabel: 'Menu'
+    }
+  }
+}, {    drawerBackgroundColor: '#D1C4E9'});
+
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({selectedDish: dishId})
+render() {
+  return(
+    <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+      <MainNavigator />
+    </View>
+  );
+}
 }
 
-  render() {
- 
-    return (
-      <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-        <MenuNavigator />
-      </View>
-    );
-  }
-}
-  
 export default Main;
