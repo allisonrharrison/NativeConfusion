c9eabb805e86993051fe6716571d8b87920b9e35
Allison Harrison <allisonrharrison@gmail.com>
2019-12-03 18:47:05 -0800

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Text, ScrollView, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
-import { LEADERS } from "../shared/leaders";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
 
 const mapStateToProps = state => {
   return {
@@ -32,7 +32,6 @@ function History() {
 }
 
 class About extends Component {
-
   static navigationOptions = {
     title: "About Us"
   };
@@ -49,18 +48,38 @@ class About extends Component {
       );
     };
 
-    return (
-      <ScrollView>
-        <History />
-        <Card title="Corporate Leadership">
-          <FlatList
-            data={this.props.leaders.leaders}
-            renderItem={renderLeader}
-            keyExtractor={item => item.id.toString()}
-          />
-        </Card>
-      </ScrollView>
-    );
+    if (this.props.leaders.isLoading) {
+      return (
+        <ScrollView>
+          <History />
+          <Card title="Corporate Leadership">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    } else if (this.props.leaders.errMess) {
+      return (
+        <ScrollView>
+          <History />
+          <Card title="Corporate Leadership">
+            <Text>{this.props.leaders.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    } else {
+      return (
+        <ScrollView>
+          <History />
+          <Card title="Corporate Leadership">
+            <FlatList
+              data={this.props.leaders.leaders}
+              renderItem={renderLeader}
+              keyExtractor={item => item.id.toString()}
+            />
+          </Card>
+        </ScrollView>
+      );
+    }
   }
 }
 
