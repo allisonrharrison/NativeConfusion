1e9bd750a99c4e86e847cd171bb8c9b15e9b01fd
Allison Harrison <allisonrharrison@gmail.com>
2019-12-19 10:09:39 -0800

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Text, ScrollView } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 class Contact extends Component {
   constructor(props) {
@@ -12,18 +13,36 @@ class Contact extends Component {
     title: "Contact Us"
   };
 
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["confusion@food.net"],
+      subject: "Enquiry",
+      body: "To whom it may concern:"
+    });
+  }
+
   render() {
     return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
-        <Card title="Contact Information">
-          <Text>121, Clear Water Bay Road</Text>
-          <Text>Clear Water Bay, Kowloon</Text>
-          <Text>HONG KONG</Text>
-          <Text>Tel: +852 1234 5678</Text>
-          <Text>Fax: +852 8765 4321</Text>
-          <Text>Email: confusion@food.net</Text>
-        </Card>
-      </Animatable.View>
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Information">
+            <Text>121, Clear Water Bay Road</Text>
+            <Text>Clear Water Bay, Kowloon</Text>
+            <Text>HONG KONG</Text>
+            <Text>Tel: +852 1234 5678</Text>
+            <Text>Fax: +852 8765 4321</Text>
+            <Text>Email: confusion@food.net</Text>
+            <Button
+              title="Send Email"
+              buttonStyle={{ backgroundColor: "#512DA8" }}
+              icon={
+                <Icon name="envelope-o" type="font-awesome" color="white" />
+              }
+              onPress={this.sendMail}
+            />
+          </Card>
+        </Animatable.View>
+      </ScrollView>
     );
   }
 }
