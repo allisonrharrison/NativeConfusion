cd4249ee6cda0016d205a02f8b2e02c5e711c3ff
Allison Harrison <allisonrharrison@gmail.com>
2019-12-19 10:12:57 -0800

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -8,7 +8,8 @@ import {
   StyleSheet,
   Button,
   Alert,
-  PanResponder
+  PanResponder,
+  Share
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -105,10 +106,7 @@ function RenderDish(props) {
           ],
           { cancelable: false }
         );
-      
-      else if (recognizeComment(gestureState))
-        props.onShowModal();
-      
+      else if (recognizeComment(gestureState)) props.onShowModal();
       else return true;
     },
     onPanResponderGrant: () => {
@@ -119,6 +117,19 @@ function RenderDish(props) {
         );
     }
   });
+
+  const shareDish = (title, message, url) => {
+    Share.share(
+      {
+        title: title,
+        message: title + ": " + message + " " + url,
+        url: url
+      },
+      {
+        dialogTitle: "Share " + title
+      }
+    );
+  };
 
   if (dish != null) {
     return (
@@ -151,6 +162,17 @@ function RenderDish(props) {
               color={"#512DA8"}
               style={styles.cardItem}
               onPress={() => props.onShowModal()}
+            />
+            <Icon
+              raised
+              reverse
+              name="share"
+              type="font-awesome"
+              color="#51D2A8"
+              style={styles.cardItem}
+              onPress={() =>
+                shareDish(dish.name, dish.description, baseUrl + dish.image)
+              }
             />
           </View>
         </Card>
